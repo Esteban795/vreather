136c20636db58febbffa75e7309acad079cbd092
Tom <61981635+BTom-GH@users.noreply.github.com>
2026-03-16 12:41:38 -0400

    feat : add a button to choose your landscape

---
+++ Assets/Scripts/EnvManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EnvManager : MonoBehaviour
+{
+    [Header("Environments")]
+    public GameObject[] environments;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        if (environments.Length == 0) return;
+        for (int i = 0; i < environments.Length; i++)
+        {
+            environments[i].SetActive(i == currentIndex); 
+        }
+    }
+    
+    public void CycleNextEnvironment()
+    {
+        if (environments.Length == 0) return;
+        environments[currentIndex].SetActive(false);
+        currentIndex = (currentIndex + 1) % environments.Length;
+        environments[currentIndex].SetActive(true);
+        Debug.Log("Nouvel environnement activé : " + environments[currentIndex].name);
+    }
+}
